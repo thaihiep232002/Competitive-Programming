141983c1f5a740807bc7afdd90b0a34543f4cf17
thaihiep232002 <thaihiep232002@gmail.com>
2022-09-24 14:07:24 +0700

    add some codes

--- leetcode/src/integers/Main.java
+++ leetcode/src/integers/Main.java
@@ -2,6 +2,6 @@ package integers;
 
 public class Main {
     public static void main(String[] args) {
-    	System.out.println(Solution.isPalindrome(321123));
-	}
+		System.out.println("Hello World");
+    }
 }
